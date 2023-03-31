9c47bb0ea61a3b276171e8cacc5b8655fa168b4f
Efim Manevich <efimmanevich@gmail.com>
2023-03-31 19:04:09 +0300

    search

--- src/lib/Finder.svelte
+++ src/lib/Finder.svelte
@@ -1,123 +1,124 @@
 <script>
-import { onMount } from 'svelte';
-import { mainObj } from '../store.js';
-export let IdDeclare
-export let setTitle
-export let hih
+  import { onMount } from "svelte";
+  import { mainObj } from "../store.js";
+  export let IdDeclare;
+  export let setTitle;
+  export let hih;
 
-let load = true
-let mid = {}
-let Descr = 'loading...'
-//mainObj.setTitle(Descr)
-let hi
-if (!hih)
-{
-  hi = document.documentElement.clientHeight - 66
-  mainObj.resize = ()=>{
-    hi = document.documentElement.clientHeight - 66
-  }
-}
-else
-  hi = hih
+  let load = true;
+  let mid = {};
+  let Descr = "loading...";
+  let findval;
 
-let current = 0
-let selectedColor = 'LightGreen'
-let grid 
+  let hi;
+  if (!hih) {
+    hi = document.documentElement.clientHeight - 66;
+    mainObj.resize = () => {
+      hi = document.documentElement.clientHeight - 66;
+    };
+  } else hi = hih;
 
-let updateTab = async () => {
- 
-  let url = mainObj.baseUrl + 'React/FinderStart'
-  let bd = new FormData();
-  bd.append('id', IdDeclare);
-  const response = await fetch(url, 
-    {
+  let current = 0;
+  let selectedColor = "LightGreen";
+  let grid;
+  let search_input;
+
+  let updateTab = async () => {
+    let url = mainObj.baseUrl + "React/FinderStart";
+    let bd = new FormData();
+    bd.append("id", IdDeclare);
+    const response = await fetch(url, {
       method: "POST",
-      body:bd
-    }
-  )
-  const data = await response.json()
-  Descr = data.Descr
-  mid = data
-  load = false
-  //grid = document.body//document.getElementById('bodytab')
-  //grid.addEventListener('keydown',(e) => {enterKeyDown(e)})
-  if (!setTitle)
-    mainObj.setTitle(Descr)
-  else
-    setTitle(Descr)  
-}
-onMount(updateTab);
+      body: bd,
+    });
+    const data = await response.json();
+    mid = data;
+    Descr = mid.Descr;
 
-let handleClick = (i) =>
-{
-  if (i == current)
-  {
-    mainObj.message()
-    return
-  }
-  
-  let maxr = mid.MainTab.length
-  if (i < 0)
-    i = 0
-  if (i >= maxr)
-    i = maxr-1
-  current = i
-}
+    load = false;
+    //grid = document.body//document.getElementById('bodytab')
+    //grid.addEventListener('keydown',(e) => {enterKeyDown(e)})
+    if (!setTitle) mainObj.setTitle(Descr, search_input);
+    else setTitle(Descr, search_input);
+  };
+  onMount(updateTab);
 
-let enterKeyDown = (event) =>
-    {
-        
-        if (event.code == "ArrowDown")
-          {
-              handleClick(current + 2);
-          }
-          if (event.code == "ArrowUp")
-          {
-              handleClick(current - 1);
-          }
-          if (event.code == "PageDown")
-          {
-              handleClick(current + 15);
-          }
-          if (event.code == "PageUp")
-          {
-              handleClick(current - 15);
-          }
-          if (event.code == "Home")
-          {
-              handleClick(0);
-          }
-          if (event.code == "End")
-          {
-              handleClick(mid.MainTab.length-1);
-          }
+  let handleClick = (i) => {
+    if (i == current) {
+      mainObj.message();
+      return;
     }
-  //document.body.addEventListener('keydown', enterKeyDown, false);  
+
+    let maxr = mid.MainTab.length;
+    if (i < 0) i = 0;
+    if (i >= maxr) i = maxr - 1;
+    current = i;
+  };
+
+  let enterKeyDown = (event) => {
+    if (event.code == "ArrowDown") {
+      handleClick(current + 2);
+    }
+    if (event.code == "ArrowUp") {
+      handleClick(current - 1);
+    }
+    if (event.code == "PageDown") {
+      handleClick(current + 15);
+    }
+    if (event.code == "PageUp") {
+      handleClick(current - 15);
+    }
+    if (event.code == "Home") {
+      handleClick(0);
+    }
+    if (event.code == "End") {
+      handleClick(mid.MainTab.length - 1);
+    }
+  };
+  //document.body.addEventListener('keydown', enterKeyDown, false);
+  let find = (e) => {
+    Descr = mid.Descr + "_" + findval;
+  };
 </script>
-<div class="overflow-auto" style="height:{hi}px">
-{#if !load}
-<table class="table table-sm" style="margin:0px">
-  <thead>
-    <tr style="position: sticky;top: 0px" class="bg-info">
-      {#each mid.Fcols as column}
-        <th scope="col">{column.FieldCaption}</th>
-      {/each}
-    </tr>
-  </thead>
-  <tbody>
-    {#each mid.MainTab as row, index}
-    <tr style="background-color:{(index==current)?selectedColor:'white'}" on:click = {()=>{handleClick(index)}}>
-      <!--<th scope="row">1</th>-->
-      {#each mid.Fcols as column}
-      <td>{row[column.FieldName]}</td>
-      {/each}
-    </tr>
-    {/each}
-    
-    
-  </tbody>
-</table>
-{/if}
+
+<div hidden>
+  <input
+    bind:this={search_input}
+    bind:value={findval}
+    type="search"
+    class="form-control"
+    placeholder="Search..."
+    aria-label="Search"
+    on:input={find}
+  />
 </div>
-
-
+<div class="overflow-auto" style="height:{hi}px">
+  {#if !load}
+    <table class="table table-sm" style="margin:0px">
+      <thead>
+        <tr style="position: sticky;top: 0px" class="bg-info">
+          {#each mid.Fcols as column}
+            <th scope="col">{column.FieldCaption}</th>
+          {/each}
+        </tr>
+      </thead>
+      <tbody>
+        {#each mid.MainTab as row, index}
+          <tr
+            style="background-color:{index == current
+              ? selectedColor
+              : 'white'}"
+            on:click={() => {
+              handleClick(index);
+            }}
+          >
+            <!--<th scope="row">1</th>-->
+            {#each mid.Fcols as column}
+              <td>{row[column.FieldName]}</td>
+            {/each}
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  {/if}
+</div>
--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -1,36 +1,41 @@
 <script>
-//https://bootstrap-4.ru/docs/5.3/components/buttons/
-//https://www.bootstrapdash.com/bootstrap-free-admin-templates
-import { onMount } from 'svelte';
-import Bar from './Bar.svelte'
-import Mymap from './Mymap.svelte'
-import Finder from './Finder.svelte'
-import {mainObj} from '../store.js'
+  //https://bootstrap-4.ru/docs/5.3/components/buttons/
+  //https://www.bootstrapdash.com/bootstrap-free-admin-templates
+  import { onMount } from "svelte";
+  import Bar from "./Bar.svelte";
+  import Mymap from "./Mymap.svelte";
+  import Finder from "./Finder.svelte";
+  import { mainObj } from "../store.js";
 
-let currentActive = 124
-mainObj.current = currentActive
-/*
-let Control = Finder
-let IdDeclare = '132'
-*/
-//set nav bsr
-let setTitle = (txt)=>
-{
-  mainObj.setTitle(txt)
-}
-let DescrMessage
-let setTitleMessage = (txt) =>
-{
-  DescrMessage = txt
-}
+  let currentActive = 124;
+  let userNav;
+  mainObj.current = currentActive;
 
+  //set nav bsr
+  let setTitle = (txt, user_con) => {
+    mainObj.setTitle(txt, user_con);
+  };
+  let DescrMessage;
+  let setTitleMessage = (txt, user_con) => {
+    DescrMessage = txt;
+    userNav.innerHTML = "";
+    if (user_con) userNav.appendChild(user_con);
+  };
 
-var myModal;
-var modalid = 'staticBackdrop';
-onMount(() => {
+  var myModal;
+  //var modalid = 'staticBackdrop';
+  let staticBackdrop;
+  onMount(() => {
+    /*
 		myModal = new bootstrap.Modal(document.getElementById('staticBackdrop'), {
                   keyboard: false});
-    mainObj.message = (msg)=>{myModal.show(msg);}
+    */
+    myModal = new bootstrap.Modal(staticBackdrop, {
+      keyboard: true,
+    });
+    mainObj.message = (msg) => {
+      myModal.show(msg);
+    };
 
     mainObj.open = (id, link1, params) => {
       /*
@@ -40,54 +45,76 @@ onMount(() => {
         Control = Mymap
       IdDeclare = params  
       */
-      currentActive = id
-      mainObj.current = currentActive
+      currentActive = id;
+      mainObj.current = currentActive;
       //25.05.2022 история по якорям
-      window.location.hash = currentActive
-    }
+      window.location.hash = currentActive;
+    };
     //история переходов
-    window.addEventListener('popstate', function() {
-      let hi = window.location.hash.replace('#', '');
-      if (hi!=mainObj.current)
-      {
+    window.addEventListener(
+      "popstate",
+      function () {
+        let hi = window.location.hash.replace("#", "");
+        if (hi != mainObj.current) {
           mainObj.current = hi;
-          currentActive = hi
-      }
-    }, false);
-    window.location.hash = currentActive
-	});
-
-
-
-
- 
+          currentActive = hi;
+        }
+      },
+      false
+    );
+    window.location.hash = currentActive;
+  });
 </script>
-<!-- Модальное окно bind:this={modalid} -->
 
-<div id = 'staticBackdrop' class="modal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+<!-- Модальное окно bind:this={modalid} -->
+<!--id = 'staticBackdrop'-->
+<div
+  bind:this={staticBackdrop}
+  class="modal"
+  data-bs-backdrop="static"
+  data-bs-keyboard="false"
+  tabindex="-1"
+  aria-labelledby="staticBackdropLabel"
+  aria-hidden="true"
+>
   <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="staticBackdropLabel">{DescrMessage}</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Закрыть"></button>
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">
+          {DescrMessage}
+        </h1>
+        <form
+          class="col-6 col-lg-auto mb-1 mb-lg-2 me-lg-1"
+          bind:this={userNav}
+        />
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="modal"
+          aria-label="Закрыть"
+        />
       </div>
       <div class="modal-body">
-        <Finder IdDeclare="132" hih="300" setTitle = {setTitleMessage}/>
+        <Finder IdDeclare="132" hih="300" setTitle={setTitleMessage} />
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Ok</button>
-        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"
+          >Ok</button
+        >
+        <button type="button" class="btn btn-primary" data-bs-dismiss="modal"
+          >Cancel</button
+        >
       </div>
     </div>
   </div>
 </div>
 
-<Bar/>
+<Bar />
 <div class="container-fluid" style="padding-top:65px;">
   <!--<svelte:component this={Control} IdDeclare = {IdDeclare}/>-->
   <!--<Finder IdDeclare = {IdDeclare}/>-->
 
-<!--
+  <!--
 <div class="container-fluid" hidden={currentActive!=1}>
 <Hdrs/>
 </div>
@@ -96,26 +123,26 @@ onMount(() => {
 </div>
 -->
 
-{#if currentActive==2}
-  <Mymap setTitle = {setTitle}/>
-{/if}
+  {#if currentActive == 2}
+    <Mymap {setTitle} />
+  {/if}
 
-{#if currentActive==124 }
-<Finder IdDeclare="132" setTitle = {setTitle}/>
-{/if}
-{#if currentActive==121 }
-<Finder IdDeclare="129"/>
-{/if}
-{#if currentActive==122 }
-<Finder IdDeclare="130"/>
-{/if}
-{#if currentActive==123 }
-<Finder IdDeclare="131"/>
-{/if}
-{#if currentActive==19 }
-<Finder IdDeclare="120"/>
-{/if}
-{#if currentActive==32 }
-<Finder IdDeclare="121"/>
-{/if}
+  {#if currentActive == 124}
+    <Finder IdDeclare="132" {setTitle} />
+  {/if}
+  {#if currentActive == 121}
+    <Finder IdDeclare="129" />
+  {/if}
+  {#if currentActive == 122}
+    <Finder IdDeclare="130" />
+  {/if}
+  {#if currentActive == 123}
+    <Finder IdDeclare="131" />
+  {/if}
+  {#if currentActive == 19}
+    <Finder IdDeclare="120" />
+  {/if}
+  {#if currentActive == 32}
+    <Finder IdDeclare="121" />
+  {/if}
 </div>
